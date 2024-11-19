f03f61d247b057aee63896a24c51b850c6d20ffd
tricha13 <tricha13@enet.meridiancc.edu>
2024-11-19 15:55:29 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 // Lab: Project1
 // Due Date: 11/21/24
 // Filename: main.cpp
-// Description: This program will ... 
+// Description: This program calculates the number of boxes cookies needed for 1 container. If the required amount of boxes are not reached, the program will discard and show leftover cookies.
+
 
 #include <iostream>
 using namespace std;
@@ -11,7 +12,7 @@ int main () {
   int cookiesTotal, boxes, leftoverCookies, containers, leftoverBoxes;
 
   //first off, get the user to input the total amount of cookies in the boxes.
-  cout << "Enter the total number of cookies";
+  cout << "Enter the total number of cookies: ";
   cin >> cookiesTotal;
 
   // calculations for the output - 75 boxes and 24 cookies
@@ -24,19 +25,19 @@ int main () {
 
   //boxes
   if (boxes > 0)
-  cout << "Boxes needed = " << boxes << endl;
+  cout << "The number of cookie boxes needed to hold the cookies: " << boxes << endl;
 
   //boxes left
   if (leftoverBoxes > 0)
-    cout << "leftover boxes = " << leftoverBoxes << endl;
+    cout << "leftover boxes: " << leftoverBoxes << endl;
 
   //containers
   if (containers > 0) 
-    cout << "Containers needed = " << containers << endl;
+    cout << "The number of containers needed to store the cookie boxes: " << containers << endl;
 
   //cookie 
   if (leftoverCookies > 0)
-    cout << "Leftover cookies = " << leftoverCookies << endl;
+    cout << "Leftover cookies: " << leftoverCookies << endl;
 
   return 0;
 }
