39a85767a8851ca9d29d4bef38820a2aeb3b3eab
tricha13 <tricha13@enet.meridiancc.edu>
2024-11-19 15:32:52 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,32 @@
 #include <iostream>
 using namespace std;
 int main () {
+  //input the variables 
+  int cookiesTotal, boxes, leftoverCookies, containers, leftoverBoxes;
+
   //first off, get the user to input the total amount of cookies in the boxes.
   cout << "Enter the total number of cookies";
   cin << cookiesTotal;
 
-  //
+  // calculations for the output - 75 boxes and 24 cookies
+  boxes = cookiesTotal / 24;
+  leftoverCookies = cookiesTotal % 24;
 
+  //75 boxes
+  leftoverBoxes = boxes % 75;
+  containers = boxes / 75;
+
+  //boxes
+  if (containers > 0)
+  cout << "leftover boxes = " << leftoverBoxes << endl;
+
+  //containers
+  if (containers > 0) 
+    cout << "Leftover containers = " << leftoverContainers << endl;
+
+  //cookie 
+  if (leftoverCookies > 0)
+    cout << "Leftover cookies = " << leftoverCookies << endl;
+
+  return 0;
+}
