e84accbe8feb9b74ed2b9f10ed41ec3a63b60e55
tricha13 <tricha13@enet.meridiancc.edu>
2024-11-19 14:09:23 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+// Name: Thomas Richards 
+// Lab: Project1
+// Due Date: 11/21/24
+// Filename: main.cpp
+// Description: This program will ... 
+
+#include <iostream>
+using namespace std;
+int main () {
+  //first off, get the user to input the total amount of cookies in the boxes.
+  cout << "Enter the total number of cookies";
+  cin << cookiesTotal;
+
+  //
 
